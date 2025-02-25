d139396d57526a27e832b5b2632eec3f352f2330
hendisantika <hendisantika@yahoo.co.id>
2025-02-25 07:50:54 +0700

    fix: List Users🫸🌀✏️📗🐧🐳⬆

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::latest()->paginate(10);
+        return view('users.index', compact('users'));
     }
 
     /**
